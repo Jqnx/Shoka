25d00a02d514e04b310a51e607c05ae92e9e8ece
Jan Van Look <jan.derpingguys@hotmail.com>
2025-04-02 01:30:22 +0200

    add custom errors

--- internal/server/errors.go
+++ internal/server/errors.go
@@ -1,9 +1,24 @@
 package server
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/go-playground/validator/v10"
+)
+
+var (
+	// Archives
+	ErrArchiveNotFound     = errors.New("archive not found")
+	ErrArchiveNoDuplicates = errors.New("archive already exists")
+
+	// Artists
+	ErrArtistNotFound     = errors.New("artist not found")
+	ErrArtistNoDuplicates = errors.New("artist already exists")
+
+	// Group
+	ErrGroupNotFound     = errors.New("group not found")
+	ErrGroupNoDuplicates = errors.New("group already exists")
 )
 
 func Validate(verr validator.ValidationErrors) map[string]string {
