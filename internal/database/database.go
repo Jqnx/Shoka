50059794e57ff06ef423b16d501a1c8a41eb2232
Jan Van Look <jan.derpingguys@hotmail.com>
2025-04-02 01:23:39 +0200

    move from gorm to pgx + sqlc

--- internal/database/database.go
+++ internal/database/database.go
@@ -1,19 +1,19 @@
 package database
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"os"
 
+	"github.com/jackc/pgx/v5"
 	_ "github.com/jackc/pgx/v5/stdlib"
 	_ "github.com/joho/godotenv/autoload"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
 )
 
 type service struct {
-	db *gorm.DB
+	// db *gorm.DB
+	db *pgx.Conn
 }
 
 var (
@@ -26,18 +26,27 @@ var (
 	dbInstance *service
 )
 
-func New() *gorm.DB {
+//func New() *gorm.DB {
+//	connStr := fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s sslmode=disable TimeZone=Europe/Brussels", username, password, host, port, database)
+//	newLogger := logger.New(
+//		log.New(os.Stdout, "\r\n", log.LstdFlags),
+//		logger.Config{
+//			Colorful: true,
+//		})
+//	db, err := gorm.Open(postgres.Open(connStr), &gorm.Config{
+//		Logger: newLogger,
+//		// FullSaveAssociations: false,
+//		TranslateError: true,
+//	})
+//	if err != nil {
+//		log.Fatal("Error connecting to database:", err)
+//	}
+//	return db
+//}
+
+func New() *pgx.Conn {
 	connStr := fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s sslmode=disable TimeZone=Europe/Brussels", username, password, host, port, database)
-	newLogger := logger.New(
-		log.New(os.Stdout, "\r\n", log.LstdFlags),
-		logger.Config{
-			Colorful: true,
-		})
-	db, err := gorm.Open(postgres.Open(connStr), &gorm.Config{
-		Logger: newLogger,
-		// FullSaveAssociations: false,
-		TranslateError: true,
-	})
+	db, err := pgx.Connect(context.Background(), connStr)
 	if err != nil {
 		log.Fatal("Error connecting to database:", err)
 	}
