1677595b7bfef13a802c3a103718f3985f0f150f
Jan Van Look <jan.derpingguys@hotmail.com>
2025-04-13 19:04:14 +0200

    ref: moved server init to main.go

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,54 +1,43 @@
 package server
 
 import (
-	"Shoka/internal/database"
-	"Shoka/internal/logger"
+	"Shoka/internal/config"
 	"Shoka/internal/repository"
-	"Shoka/internal/store"
 	"fmt"
+	"log/slog"
 	"net/http"
-	"os"
 	"strconv"
 	"time"
 
 	"github.com/jackc/pgx/v5"
 	_ "github.com/joho/godotenv/autoload"
+	"riverqueue.com/riverui"
 )
 
 type Server struct {
-	port  int
-	store store.Storage
-	repo  *repository.Queries
-	db    *pgx.Conn
-	log   logger.Logger
+	port     int
+	repo     *repository.Queries
+	db       *pgx.Conn
+	log      *slog.Logger
+	workerui *riverui.Server
 }
 
-func NewServer() *http.Server {
-	l := logger.Get()
-	port, _ := strconv.Atoi(os.Getenv("PORT"))
-	db := database.New()
-	l.Logger.Info().Msg("Connected to database.")
-	repo := repository.New(db)
+func NewServer(
+	config *config.Config,
+	db *pgx.Conn,
+	repo *repository.Queries,
+	log *slog.Logger,
+	// workerui *riverui.Server,
+
+) *http.Server {
+	port, _ := strconv.Atoi(config.Server.Port)
 	NewServer := &Server{
 		port: port,
-		// store:  store,
 		repo: repo,
 		db:   db,
-		log:  l,
+		log:  log,
+		// workerui: workerui,
 	}
-
-	// Auto Migrations
-	// store.Migrate.Migrate()
-	// logger.Info("Migrations done.")
-
-	// Scan for files
-	//logger.Info("Scanning for files...")
-	//err := store.Archive.CreateFromFile()
-	//if err != nil {
-	//	logger.Error(err)
-	//} else {
-	//	logger.Info("Scanned Successfully.")
-	//}
 
 	// Declare Server config
 	server := &http.Server{
