4857db5cf8860f22cf09b875fd25059f740b4afb
Jan Van Look <jan.derpingguys@hotmail.com>
2025-04-13 19:01:11 +0200

    feat: added metadata creation in archive transaction and creating archives from file

--- internal/archive/create.go
+++ internal/archive/create.go
@@ -1,9 +1,12 @@
 package archive
 
 import (
+	"Shoka/internal/config"
+	"Shoka/internal/fsutil"
 	"Shoka/internal/models"
 	"Shoka/internal/repository"
 	"context"
+	"log/slog"
 	"strings"
 	"time"
 
@@ -11,33 +14,119 @@ import (
 )
 
 // TODO:
-// Archive Metadata Queries
-// Create Archive Transaction with metadata
 
-func CreateTransaction(ctx context.Context,
+func CreateTransaction(c context.Context,
 	db *pgx.Conn,
-	queries *repository.Queries,
+	q *repository.Queries,
 	payload *models.ArchivePayload,
-) (*repository.Archive, error) {
-	tx, err := db.Begin(ctx)
+	log *slog.Logger,
+) (*models.ArchiveResponse, error) {
+	tx, err := db.Begin(c)
 	if err != nil {
+		log.Error(err.Error())
 		return nil, err
 	}
-	defer tx.Rollback(ctx)
-	qtx := queries.WithTx(tx)
+	defer tx.Rollback(c)
+	qtx := q.WithTx(tx)
+
+	aid, err := qtx.GetArchiveLastAID(c)
+	if err != nil {
+		if err == pgx.ErrNoRows {
+			aid = 0
+		} else {
+			log.Error(err.Error())
+			return nil, err
+		}
+	}
+
 	lang := strings.ToLower(payload.Language)
 	category := strings.ToLower(payload.Category)
-	archive, err := qtx.CreateArchive(ctx, repository.CreateArchiveParams{
+	archive, err := qtx.CreateArchive(c, repository.CreateArchiveParams{
 		Title:     payload.Title,
 		Summary:   &payload.Summary,
 		Lang:      &lang,
 		Category:  &category,
 		FilePath:  &payload.FilePath,
+		AID:       aid + 1,
 		CreatedAt: time.Now(),
 		UpdatedAt: time.Now(),
 	})
 	if err != nil {
+		log.Error(err.Error())
 		return nil, err
 	}
-	return &archive, tx.Commit(ctx)
+
+	if err := Artist(c, qtx, payload, &archive); err != nil {
+		log.Error(err.Error())
+		return nil, err
+	}
+
+	if err := Tag(c, qtx, payload, &archive); err != nil {
+		log.Error(err.Error())
+		return nil, err
+	}
+
+	if err := Character(c, qtx, payload, &archive); err != nil {
+		log.Error(err.Error())
+		return nil, err
+	}
+
+	if err := Parody(c, qtx, payload, &archive); err != nil {
+		log.Error(err.Error())
+		return nil, err
+	}
+
+	if err := URL(c, qtx, payload, &archive); err != nil {
+		log.Error(err.Error())
+		return nil, err
+	}
+
+	result, err := Get(c, qtx, archive.AID, log)
+	if err != nil {
+		log.Error(err.Error())
+		return nil, err
+	}
+
+	return result, tx.Commit(c)
+}
+
+func CreateFromFile(c context.Context, path string, q *repository.Queries, log *slog.Logger) error {
+	if fsutil.MatchExtension(path, config.ArchiveExtensions) {
+		aid, err := q.GetArchiveLastAID(c)
+		if err != nil {
+			if err == pgx.ErrNoRows {
+				aid = 0
+			} else {
+				log.Error(err.Error())
+				return err
+			}
+		}
+
+		exists, err := q.FilePathExists(c, &path)
+		if err != nil {
+			log.ErrorContext(c, "error checking if file is already in db")
+		}
+		if exists.RowsAffected() == 0 {
+			title := fsutil.GetNameFromPath(path, true)
+			pagecount := fsutil.GetPageCount(path, config.ImageExtensions)
+
+			_, err = q.CreateArchive(c, repository.CreateArchiveParams{
+				Title:     title,
+				PageCount: pagecount,
+				FilePath:  &path,
+				AID:       aid + 1,
+				CreatedAt: time.Now(),
+				UpdatedAt: time.Now(),
+			})
+			if err != nil {
+				log.ErrorContext(c, err.Error())
+				return err
+			}
+			log.Info(title)
+		}
+	} else {
+		title := fsutil.GetNameFromPath(path, true)
+		log.Warn("failed to create", "archive", title)
+	}
+	return nil
 }
