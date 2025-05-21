d89750e717ec49551be2f26d451d94a2aec8199b
Jan Van Look <jan.derpingguys@hotmail.com>
2025-05-21 03:18:26 +0200

    create notify funtion

--- migrations/00001_create_archive_table.sql
+++ migrations/00001_create_archive_table.sql
@@ -1,18 +1,27 @@
 -- +goose Up
 CREATE TABLE IF NOT EXISTS archives (
     id bigserial PRIMARY KEY,
-    title text UNIQUE NOT NULL,
+    title text NOT NULL,
     summary text,
-    lang varchar(2),
-    category varchar(10),
+    language varchar(2),
+    category text,
     page_count bigint NOT NULL DEFAULT 0,
     file_path text UNIQUE,
-    a_id bigint NOT NULL DEFAULT 1,
+    archive_id text NOT NULL,
+    hash text,
+    thumbs_path text,
+    cover_path text,
+-- pages_path text,
+    type text NOT NULL,
     created_at timestamptz NOT NULL DEFAULT NOW(),
     updated_at timestamptz NOT NULL
 );
 
-create unique index idx_a_id on archives(a_id);
+create unique index idx_archive_id on archives(archive_id);
+create unique index idx_thumbs_path on archives(thumbs_path);
+create unique index idx_cover_path on archives(cover_path);
+create unique index idx_hash on archives(hash);
+create index idx_title on archives(title);
 
 -- +goose Down
 DROP TABLE IF EXISTS archives;
---
+++ migrations/00015_create_notify_function.sql
@@ -0,0 +1,27 @@
+-- +goose Up
+-- +goose StatementBegin
+create function notify_archives()
+returns trigger
+as $$
+DECLARE
+BEGIN
+  PERFORM pg_notify('notifyarchives', to_jsonb(NEW)::text);
+  RETURN NEW;
+END;
+$$
+language plpgsql
+;
+
+CREATE TRIGGER archive_insert 
+AFTER INSERT ON archives FOR EACH ROW EXECUTE PROCEDURE notify_archives();
+
+-- +goose StatementEnd
+-- +goose Down
+-- +goose StatementBegin
+drop trigger archive_insert on archives;
+drop function notify_archives()
+;
+
+-- +goose StatementEnd
+
+
