063febecf11c7f88493e0734be2f0e989626fce4
Jan Van Look <jan.derpingguys@hotmail.com>
2025-05-21 03:35:20 +0200

    added cover handlers

---
+++ internal/server/cover.go
@@ -0,0 +1,79 @@
+package server
+
+import (
+	"Shoka/internal/models"
+	"Shoka/internal/workers"
+	"context"
+	"fmt"
+	"net/http"
+	"path/filepath"
+
+	"github.com/gin-gonic/gin"
+	"github.com/hibiken/asynq"
+)
+
+func (s *Server) generateCoverHandler(c *gin.Context) {
+	id := c.Param("id")
+	force := c.Query("force")
+
+	ctx := context.Background()
+
+	arch, err := s.repo.GetArchiveByID(ctx, id)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, &models.ResponseError{
+			Status:  "error",
+			Message: err.Error(),
+		})
+		return
+	}
+
+	ch := make(chan *asynq.TaskInfo)
+
+	if force == "true" {
+		w := workers.NewWorkers(s.app, true, ctx)
+		go w.Covers(ch, &arch)
+		cover := <-ch
+
+		// Sets Location header to URI of job status
+		c.Header("Location", fmt.Sprintf("/api/status/%s", cover.ID))
+	} else {
+		w := workers.NewWorkers(s.app, false, ctx)
+		go w.Covers(ch, &arch)
+		cover := <-ch
+		c.Header("Location", fmt.Sprintf("/api/status/%s", cover.ID))
+	}
+}
+
+func (s *Server) getCoverHandler(c *gin.Context) {
+	id := c.Param("id")
+
+	ctx := context.Background()
+
+	arch, err := s.repo.GetArchiveByID(ctx, id)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, &models.ResponseError{
+			Status:  "error",
+			Message: err,
+		})
+		return
+	}
+
+	if arch.ThumbsPath == nil {
+		c.JSON(http.StatusInternalServerError, &models.ResponseError{
+			Status:  "error",
+			Message: "no cover found",
+		})
+		return
+	}
+
+	if *arch.ThumbsPath == "" {
+		c.JSON(http.StatusInternalServerError, &models.ResponseError{
+			Status:  "error",
+			Message: "no cover found",
+		})
+		return
+	}
+
+	file := filepath.Join(*arch.ThumbsPath, "cover", *arch.CoverPath)
+	c.File(file)
+}
