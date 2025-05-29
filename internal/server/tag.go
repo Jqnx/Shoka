ac837507f0be50382f9c8180a9da8a484a3f47d7
Jan Van Look <jan.derpingguys@hotmail.com>
2025-05-29 16:45:01 +0200

    implement pagination for tags

---
+++ internal/server/tag.go
@@ -0,0 +1,146 @@
+package server
+
+import (
+	"Shoka/internal/config"
+	"Shoka/internal/models"
+	"Shoka/internal/repository"
+	"context"
+	"net/http"
+	"strconv"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5"
+)
+
+func (s *Server) getAllTagHandler(c *gin.Context) {
+	ctx := context.Background()
+
+	tags, err := s.repo.GetAllTags(ctx)
+	if err != nil {
+		if err == pgx.ErrNoRows {
+			c.JSON(http.StatusNotFound, &models.ResponseError{
+				Status:  "error",
+				Message: config.ErrNoTags.Error(),
+			})
+			return
+		} else {
+			c.JSON(http.StatusInternalServerError, &models.ResponseError{
+				Status:  "error",
+				Message: err.Error(),
+			})
+			return
+		}
+	}
+
+	c.JSON(http.StatusOK, tags)
+}
+
+func (s *Server) getArchiveByTagHandler(c *gin.Context) {
+	// get tag param & pagination queries
+	tag := strings.ToLower(c.Param("tag"))
+	p := c.Query("page")
+	ps := c.Query("size")
+
+	// Creates context
+	ctx := context.Background()
+
+	// Checks if tag exists
+	exists, err := s.repo.TagExists(ctx, tag)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	// If tag does not exists respond with 404 ErrTagNotFound
+	if exists.RowsAffected() == 0 {
+		c.JSON(http.StatusNotFound, &models.ResponseError{
+			Status:  "error",
+			Message: config.ErrTagNotFound.Error(),
+		})
+		return
+	}
+
+	// Get archives
+
+	if p == "" && ps == "" {
+		archives, err := s.repo.GetArchivesByTag(ctx, tag)
+		if err != nil {
+			// If no archives found respond with 404 ErrNoArchive
+			if err == pgx.ErrNoRows {
+				c.JSON(http.StatusNotFound, &models.ResponseError{
+					Status:  "error",
+					Message: config.ErrNoArchive.Error(),
+				})
+				return
+			} else {
+				// Otherwise respond with 500 and error
+				c.JSON(http.StatusInternalServerError, &models.ResponseError{
+					Status:  "error",
+					Message: err.Error(),
+				})
+				return
+			}
+		}
+
+		count, err := s.repo.CountArchivesWithTag(ctx, tag)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, &models.ResponseError{
+				Status:  "error",
+				Message: err.Error(),
+			})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"archives": archives,
+			"total":    count,
+		})
+
+	} else {
+
+		page, _ := strconv.Atoi(p)
+		if page == 0 {
+			page = 1
+		}
+		pageSize, _ := strconv.Atoi(ps)
+		if pageSize == 0 {
+			pageSize = 10
+		}
+
+		archives, err := s.repo.GetArchivesByTagList(ctx, repository.GetArchivesByTagListParams{
+			Tag:    tag,
+			Offset: (int32(page) - 1) * int32(pageSize),
+			Limit:  int32(pageSize),
+		})
+		if err != nil {
+			// If no archives found respond with 404 ErrNoArchive
+			if err == pgx.ErrNoRows {
+				c.JSON(http.StatusNotFound, &models.ResponseError{
+					Status:  "error",
+					Message: config.ErrNoArchive.Error(),
+				})
+				return
+			} else {
+				// Otherwise respond with 500 and error
+				c.JSON(http.StatusInternalServerError, &models.ResponseError{
+					Status:  "error",
+					Message: err.Error(),
+				})
+				return
+			}
+		}
+		count, err := s.repo.CountArchivesWithTag(ctx, tag)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, &models.ResponseError{
+				Status:  "error",
+				Message: err.Error(),
+			})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"archives": archives,
+			"total":    count,
+		})
+	}
+}
