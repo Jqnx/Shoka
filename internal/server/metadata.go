5c7b5019cbe771cfc78d5829dae320b5067295e4
Jan Van Look <jan.derpingguys@hotmail.com>
2025-05-29 16:45:14 +0200

    add metadata scan handler

---
+++ internal/server/metadata.go
@@ -0,0 +1,39 @@
+package server
+
+import (
+	"Shoka/internal/config"
+	"Shoka/internal/models"
+	"Shoka/internal/workers"
+	"context"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5"
+)
+
+func (s *Server) scanMetadataHandler(c *gin.Context) {
+	ctx := context.Background()
+	id := c.Param("id")
+	src := c.Query("source")
+
+	archive, err := s.repo.GetArchiveByID(ctx, id)
+	if err != nil {
+		if err == pgx.ErrNoRows {
+			c.JSON(http.StatusNotFound, &models.ResponseError{
+				Status:  "error",
+				Message: config.ErrArchiveNotFound.Error(),
+			})
+			return
+		} else {
+			c.JSON(http.StatusInternalServerError, &models.ResponseError{
+				Status:  "error",
+				Message: err.Error(),
+			})
+			return
+		}
+	}
+
+	w := workers.NewWorkers(s.app, false, ctx)
+	go w.Metadata(&archive, src)
+	c.JSON(http.StatusOK, "success")
+}
