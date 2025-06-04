906ce091f9bf28c48c09131dfda4bc8fc3a59df2
Jan Van Look <jan.derpingguys@hotmail.com>
2025-06-04 18:19:10 +0200

    frontend: add toaster

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,10 +1,11 @@
-<script>
-
-
+<script setup lang="ts">
+  import { Toaster } from "./components/ui/sonner";
+  import "vue-sonner/style.css";
 </script>
 
 <template>
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
+  <Toaster />
 </template>
