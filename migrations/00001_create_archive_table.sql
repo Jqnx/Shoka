55b11e423ccb3ecac131b156e6a98e1591307cf7
Jan Van Look <jan.derpingguys@hotmail.com>
2025-06-06 18:10:58 +0200

    add search_vector + index for FTS

--- migrations/00001_create_archive_table.sql
+++ migrations/00001_create_archive_table.sql
@@ -15,7 +15,8 @@ CREATE TABLE IF NOT EXISTS archives (
     type text NOT NULL,
     created_at timestamptz NOT NULL,
     updated_at timestamptz NOT NULL,
-    release_date timestamptz
+    release_date timestamptz,
+    search_vector tsvector
 );
 
 create unique index idx_archive_id on archives(archive_id);
@@ -23,6 +24,7 @@ create unique index idx_thumbs_path on archives(thumbs_path);
 create unique index idx_cover_path on archives(cover_path);
 create unique index idx_hash on archives(hash);
 create index idx_title on archives(title);
+create index idx_search_vector on archives using gin(search_vector);
 
 -- +goose Down
 DROP TABLE IF EXISTS archives;
