07cc4be02d6332137ebb581c27b866a1c433930a
Jan Van Look <jan.derpingguys@hotmail.com>
2025-06-18 02:28:17 +0200

    added function to match strings in 2 different slices

--- internal/util/slices.go
+++ internal/util/slices.go
@@ -1,6 +1,8 @@
 package util
 
-import "slices"
+import (
+	"slices"
+)
 
 func RemoveDuplicatesStrPointer(in []*string) []string {
 	var list []string
@@ -9,4 +11,18 @@ func RemoveDuplicatesStrPointer(in []*string) []string {
 	}
 	slices.Sort(list)
 	return slices.Compact(list)
+}
+
+func MatchStringsInSlices(in1 []string, in2 []string) []string {
+	var list []string
+	for _, i := range in1 {
+		if in2 != nil {
+			if slices.Contains(in2, i) {
+				list = append(list, i)
+			}
+		} else {
+			return in1
+		}
+	}
+	return list
 }
