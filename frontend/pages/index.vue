655c5f4542ead31a58ba9407ae2655756c514eb6
Jan Van Look <jan.derpingguys@hotmail.com>
2025-07-01 21:08:12 +0200

    frontend: ref

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -8,9 +8,7 @@
 <template>
   <div class="flex flex-col gap-4 p-4 pt-0">
     <div class="flex flex-col gap-2">
-      <CarouselTitle
-        title="Recent Doujinshi"
-        :to="`/a`" />
+      <CarouselTitle title="Recent Doujinshi" to="a" />
       <ArchiveCarousel />
     </div>
   </div>
