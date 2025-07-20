ec2feb86b17dca78d9695779b9f0cb3ae1a40ec9
Jan Van Look <jan.derpingguys@hotmail.com>
2025-07-20 18:28:58 +0200

    frontend: getting rid of even more spacing

--- frontend/app/pages/parody/[parody]/index.vue
+++ frontend/app/pages/parody/[parody]/index.vue
@@ -28,7 +28,7 @@
   <div class="flex flex-1 flex-col gap-4 p-4 pt-0">
     <!--TODO: Filter options here -->
     <div
-      class="px-16 py-4 grid gap-4 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+      class="py-4 grid gap-4 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
       <div v-for="archive in archives.archives" :key="archive.id">
         <GalleryItem :id="archive.archive_id" :title="archive.title" />
       </div>
