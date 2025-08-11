ace0194a1239ce0ae32972ec7479db4dbd3952a2
Jan Van Look <jan.derpingguys@hotmail.com>
2025-08-11 19:27:34 +0200

    added function to create all necessary dirs

---
+++ internal/fsutil/dirs.go
@@ -0,0 +1,22 @@
+package fsutil
+
+import (
+	"Shoka/internal/config"
+	"os"
+)
+
+func CreateDirs(cfg *config.Config) error {
+	if err := os.MkdirAll(cfg.ContentDir, 0755); err != nil {
+		return err
+	}
+
+	if err := os.MkdirAll(cfg.DownloadDir, 0755); err != nil {
+		return err
+	}
+
+	if err := os.MkdirAll(cfg.ThumbDir, 0755); err != nil {
+		return err
+	}
+
+	return nil
+}
