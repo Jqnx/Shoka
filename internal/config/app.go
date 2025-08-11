7008c524a6e32c38551aad9f7132b50e5ce85658
Jan Van Look <jan.derpingguys@hotmail.com>
2025-08-11 19:27:15 +0200

    added client & websockets hub to App

--- internal/config/app.go
+++ internal/config/app.go
@@ -4,24 +4,43 @@ import (
 	"Shoka/internal/logger"
 	"Shoka/internal/notifier"
 	"Shoka/internal/repository"
+	"Shoka/internal/websocket"
 
+	"github.com/hibiken/asynq"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 type App struct {
-	Repo *repository.Queries
-	Log  logger.Logger
-	DB   *pgxpool.Pool
-	Cfg  *Config
-	Noti notifier.Notifier
+	Repo   *repository.Queries
+	Log    logger.Logger
+	DB     *pgxpool.Pool
+	Cfg    *Config
+	Noti   notifier.Notifier
+	Client *asynq.Client
+	Hub    *websocket.Hub
 }
 
-func NewApp(repo *repository.Queries, log logger.Logger, db *pgxpool.Pool, cfg *Config, noti notifier.Notifier) App {
+func NewApp(repo *repository.Queries,
+	log logger.Logger,
+	db *pgxpool.Pool,
+	cfg *Config,
+	noti notifier.Notifier,
+	client *asynq.Client,
+	hub *websocket.Hub,
+) App {
 	return App{
-		Repo: repo,
-		Log:  log,
-		DB:   db,
-		Cfg:  cfg,
-		Noti: noti,
+		Repo:   repo,
+		Log:    log,
+		DB:     db,
+		Cfg:    cfg,
+		Noti:   noti,
+		Client: client,
+		Hub:    hub,
+	}
+}
+
+func (a *App) Close() {
+	if a.Client != nil {
+		a.Client.Close()
 	}
 }
