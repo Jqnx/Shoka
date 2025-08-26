fa212ec449ba4b9946e96b1d39804ec52967df74
Jan Van Look <jan.derpingguys@hotmail.com>
2025-08-27 01:43:14 +0200

    frontend: fixed progress (page_count => page-count)

--- frontend/app/pages/favorites/index.vue
+++ frontend/app/pages/favorites/index.vue
@@ -1,58 +1,58 @@
 <script setup lang="ts">
-  import {
-    Pagination,
-    PaginationContent,
-    PaginationEllipsis,
-    PaginationItem,
-    PaginationNext,
-    PaginationPrevious,
-    PaginationFirst,
-    PaginationLast,
-  } from "@/components/ui/pagination";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationNext,
+  PaginationPrevious,
+  PaginationFirst,
+  PaginationLast,
+} from "@/components/ui/pagination";
 
-  useHead({
-    title: "Favorites",
-  });
+useHead({
+  title: "Favorites",
+});
 
-  const { currentPage, pageSize } = storeToRefs(usePageStore());
-  const { sortBy, sortDir, filters } = storeToRefs(useFavoriteFiltersStore());
+const { currentPage, pageSize } = storeToRefs(usePageStore());
+const { sortBy, sortDir, filters } = storeToRefs(useFavoriteFiltersStore());
 
-  const { token } = useAuth();
+const { token } = useAuth();
 
-  const { data: archives } = await useAsyncData(
-    "favorites",
-    () =>
-      $fetch("/api/user/favorites", {
-        method: "POST",
-        onRequest({ options }) {
-          options.headers.set("Authorization", `${token.value}`);
-        },
-        query: {
-          page: currentPage.value,
-          size: pageSize.value,
-          sortby: sortBy.value,
-          sortdir: sortDir.value,
-        },
-        body: {
-          tags: filters.value.tags,
-          artists: filters.value.artists,
-          characters: filters.value.characters,
-          parodies: filters.value.parodies,
-          languages: filters.value.languages,
-          categories: filters.value.categories,
-        },
-      }),
-    { watch: [sortBy, sortDir, currentPage] }
-  );
+const { data: archives } = await useAsyncData(
+  "favorites",
+  () =>
+    $fetch("/api/user/favorites", {
+      method: "POST",
+      onRequest({ options }) {
+        options.headers.set("Authorization", `${token.value}`);
+      },
+      query: {
+        page: currentPage.value,
+        size: pageSize.value,
+        sortby: sortBy.value,
+        sortdir: sortDir.value,
+      },
+      body: {
+        tags: filters.value.tags,
+        artists: filters.value.artists,
+        characters: filters.value.characters,
+        parodies: filters.value.parodies,
+        languages: filters.value.languages,
+        categories: filters.value.categories,
+      },
+    }),
+  { watch: [sortBy, sortDir, currentPage] },
+);
 
-  const scrollToTop = () => {
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  };
+const scrollToTop = () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+};
 
-  const router = useRouter();
-  router.beforeResolve((_) => {
-    currentPage.value = 1;
-  });
+const router = useRouter();
+router.beforeResolve((_) => {
+  currentPage.value = 1;
+});
 </script>
 
 <template>
@@ -60,13 +60,15 @@
     <!--TODO: Filter options here -->
     <ListOptions />
     <div
-      class="py-4 grid gap-4 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+      class="py-4 grid gap-4 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6"
+    >
       <div v-for="archive in archives.archives" :key="archive.id">
         <GalleryItem
           :id="archive.archive_id"
           :title="archive.title"
           :progress="archive.page"
-          :page_count="archive.page_count" />
+          :page-count="archive.page_count"
+        />
       </div>
     </div>
     <Pagination
@@ -75,7 +77,8 @@
       :sibling-count="2"
       :items-per-page="pageSize"
       :total="archives.total"
-      :default-page="1">
+      :default-page="1"
+    >
       <PaginationContent v-slot="{ items }">
         <PaginationFirst @click="scrollToTop" />
         <PaginationPrevious @click="scrollToTop" />
@@ -86,7 +89,8 @@
             :key="index"
             :value="item.value"
             :is-active="item.value == currentPage"
-            @click="scrollToTop">
+            @click="scrollToTop"
+          >
             {{ item.value }}
           </PaginationItem>
           <PaginationEllipsis v-else :key="item.type" :index="index" />
