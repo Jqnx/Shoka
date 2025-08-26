6cc47cd4fecb9e02dad31ceade9a25eb121ce3fc
Jan Van Look <jan.derpingguys@hotmail.com>
2025-08-27 01:35:33 +0200

    added nh file types

--- internal/config/sources.go
+++ internal/config/sources.go
@@ -12,10 +12,15 @@ var (
 
 	// Source Specific
 	// nhentai
-	NHDomain  = "nhentai.net"
-	NHApi     = "https://nhentai.net/api"
-	NHImages  = "https://i.nhentai.net"
-	NHGallery = "https://nhentai.net/g"
+	NHDomain    = "nhentai.net"
+	NHApi       = "https://nhentai.net/api"
+	NHImages    = "https://i.nhentai.net"
+	NHGallery   = "https://nhentai.net/g"
+	NHFileTypes = map[string]string{
+		"w": ".webp",
+		"j": ".jpg",
+		"p": ".png",
+	}
 
 	// ComicInfo
 	ComicInfoFile = "ComicInfo.xml"
