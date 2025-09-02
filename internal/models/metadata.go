1d421167c44bd21044e14d9abcae205430557e89
Jan Van Look <jan.derpingguys@hotmail.com>
2025-09-02 23:56:37 +0200

    added json tags to metadata struct

--- internal/models/metadata.go
+++ internal/models/metadata.go
@@ -5,38 +5,38 @@ import (
 )
 
 type Metadata struct {
-	Title       string
-	Summary     string
-	URL         []URL
-	Category    string
-	Parody      []Parody
-	Character   []Character
-	Tags        []Tag
-	Artist      []Artist
-	Language    string
-	ReleaseDate *time.Time
-	PageCount   int
-	NHID        int
-	NHMediaID   string
-	NHImageType string
+	Title       string      `json:"title"`
+	Summary     string      `json:"summary"`
+	URL         []URL       `json:"urls"`
+	Category    string      `json:"category"`
+	Parody      []Parody    `json:"parodies"`
+	Character   []Character `json:"characters"`
+	Tags        []Tag       `json:"tags"`
+	Artist      []Artist    `json:"artists"`
+	Language    string      `json:"language"`
+	ReleaseDate *time.Time  `json:"release_date"`
+	PageCount   int         `json:"page_count"`
+	NHID        int         `json:"nhid"`
+	NHMediaID   string      `json:"nhmediaid"`
+	NHImageType string      `json:"nhimagetype"`
 }
 
 type URL struct {
-	URL string
+	URL string `json:"url"`
 }
 
 type Parody struct {
-	Parody string
+	Parody string `json:"parody"`
 }
 
 type Character struct {
-	Character string
+	Character string `json:"character"`
 }
 
 type Tag struct {
-	Tag string
+	Tag string `json:"tag"`
 }
 
 type Artist struct {
-	Artist string
+	Artist string `json:"artist"`
 }
