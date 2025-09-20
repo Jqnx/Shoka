7787c9b722ad785606e4d4c4146bdd7116cfd5e2
Jan Van Look <jan.derpingguys@hotmail.com>
2025-09-21 01:12:43 +0200

    changed archive details badges colors to be more readable/nicer colors

--- frontend/app/components/ArchiveDetails.vue
+++ frontend/app/components/ArchiveDetails.vue
@@ -18,154 +18,134 @@ const df = new DateFormatter("en-GB", {
   <div class="flex flex-col gap-1 overflow-auto">
     <!-- Artists -->
     <div
+      v-if="archive.artist"
       class="flex gap-1.5 flex-wrap sm:gap-0 sm:grid grid-cols-6 md:grid-cols-5 2xl:grid-cols-6"
     >
       <p class="text-md font-medium text-foreground/80">Artists</p>
       <div class="flex flex-wrap gap-1 col-start-2 col-span-full">
         <Badge
-          v-if="!archive.artist"
-          class="bg-primary rounded-sm hover:bg-primary/80"
+          v-for="artist in archive.artist"
+          :key="artist.id"
+          class="bg-border text-foreground font-normal rounded-sm hover:bg-border/80"
         >
-          null
+          <NuxtLink
+            :to="{ name: 'artist-artist', params: { artist: artist.name } }"
+            >{{ artist.name }}</NuxtLink
+          >
         </Badge>
-        <div v-for="artist in archive.artist" v-else :key="artist.id">
-          <Badge class="bg-primary font-normal rounded-sm hover:bg-primary/80">
-            <NuxtLink
-              :to="{ name: 'artist-artist', params: { artist: artist.name } }"
-              >{{ artist.name }}</NuxtLink
-            >
-          </Badge>
-        </div>
       </div>
     </div>
 
     <!-- Tags -->
     <div
+      v-if="archive.tags"
       class="flex gap-1.5 flex-wrap sm:gap-0 sm:grid sm:grid-cols-6 md:grid-cols-5 2xl:grid-cols-6"
     >
       <p class="text-md font-medium text-foreground/80">Tags</p>
       <div class="flex gap-1 flex-wrap col-span-full col-start-2">
         <Badge
-          v-if="!archive.tags"
-          class="bg-primary rounded-sm hover:bg-primary/80"
+          v-for="tag in archive.tags"
+          :key="tag.id"
+          class="bg-border text-foreground font-normal rounded-sm hover:bg-border/80"
         >
-          null</Badge
-        >
-        <div v-for="tag in archive.tags" v-else :key="tag.id">
-          <Badge class="bg-primary font-normal rounded-sm hover:bg-primary/80">
-            <NuxtLink :to="{ name: 'tag-tag', params: { tag: tag.name } }">{{
-              tag.name
-            }}</NuxtLink>
-          </Badge>
-        </div>
+          <NuxtLink :to="{ name: 'tag-tag', params: { tag: tag.name } }">{{
+            tag.name
+          }}</NuxtLink>
+        </Badge>
       </div>
     </div>
 
     <!-- Parodies -->
     <div
+      v-if="archive.parody"
       class="flex gap-1.5 flex-wrap sm:grid grid-cols-6 md:grid-cols-5 2xl:grid-cols-6"
     >
       <p class="text-md font-medium text-foreground/80">Parodies</p>
       <div class="flex gap-1 flex-wrap col-start-2 col-span-full">
         <Badge
-          v-if="!archive.parody"
-          class="bg-primary rounded-sm hover:bg-primary/80"
+          v-for="parody in archive.parody"
+          :key="parody.id"
+          class="bg-border text-foreground font-normal rounded-sm hover:bg-border/80"
         >
-          null
+          <NuxtLink
+            :to="{ name: 'parody-parody', params: { parody: parody.name } }"
+            >{{ parody.name }}</NuxtLink
+          >
         </Badge>
-        <div v-for="parody in archive.parody" v-else :key="parody.id">
-          <Badge class="bg-primary font-normal rounded-sm hover:bg-primary/80">
-            <NuxtLink
-              :to="{ name: 'parody-parody', params: { parody: parody.name } }"
-              >{{ parody.name }}</NuxtLink
-            >
-          </Badge>
-        </div>
       </div>
     </div>
 
     <!-- Characters -->
     <div
+      v-if="archive.character"
       class="flex gap-1.5 flex-wrap sm:grid grid-cols-6 md:grid-cols-5 2xl:grid-cols-6"
     >
       <p class="text-md font-medium text-foreground/80">Characters</p>
       <div class="flex flex-wrap gap-1 col-start-2 col-span-full">
         <Badge
-          v-if="!archive.character"
-          class="bg-primary rounded-sm hover:bg-primary/80"
+          v-for="character in archive.character"
+          :key="character.id"
+          class="bg-border text-foreground font-normal rounded-sm hover:bg-border/80"
         >
-          null
+          <NuxtLink
+            :to="{
+              name: 'character-character',
+              params: { character: character.name },
+            }"
+            >{{ character.name }}</NuxtLink
+          >
         </Badge>
-        <div v-for="character in archive.character" v-else :key="character.id">
-          <Badge class="bg-primary font-normal rounded-sm hover:bg-primary/80">
-            <NuxtLink
-              :to="{
-                name: 'character-character',
-                params: { character: character.name },
-              }"
-              >{{ character.name }}</NuxtLink
-            >
-          </Badge>
-        </div>
       </div>
     </div>
 
     <!-- Languages -->
     <div
+      v-if="archive.language"
       class="flex gap-1.5 sm:grid grid-cols-6 md:grid-cols-5 2xl:grid-cols-6"
     >
       <p class="text-md font-medium text-foreground/80">Languages</p>
       <div class="flex flex-wrap gap-1 col-start-2 col-span-full">
         <Badge
-          v-if="!archive.language"
-          class="bg-primary rounded-sm hover:bg-primary/80"
+          class="bg-border text-foreground font-normal rounded-sm hover:bg-border/80"
         >
-          null
+          <NuxtLink
+            :to="{ name: 'lang-lang', params: { lang: archive.language } }"
+            >{{ archive.language }}</NuxtLink
+          >
         </Badge>
-        <div v-else :key="archive.archive_id">
-          <Badge class="bg-primary font-normal rounded-sm hover:bg-primary/80">
-            <NuxtLink
-              :to="{ name: 'lang-lang', params: { lang: archive.language } }"
-              >{{ archive.language }}</NuxtLink
-            >
-          </Badge>
-        </div>
       </div>
     </div>
 
     <!-- Categories -->
     <div
+      v-if="archive.category"
       class="flex gap-1.5 sm:grid grid-cols-6 md:grid-cols-5 2xl:grid-cols-6"
     >
       <p class="text-md font-medium text-foreground/80">Categories</p>
       <div class="flex flex-wrap gap-1 col-start-2 col-span-full">
         <Badge
-          v-if="!archive.category"
-          class="bg-primary rounded-sm hover:bg-primary/80"
+          class="bg-border text-foreground font-normal rounded-sm hover:bg-border/80"
         >
-          null
+          <NuxtLink
+            :to="{
+              name: 'category-category',
+              params: { category: archive.category },
+            }"
+            >{{ archive.category }}</NuxtLink
+          >
         </Badge>
-        <div v-else :key="archive.archive_id">
-          <Badge class="bg-primary font-normal rounded-sm hover:bg-primary/80">
-            <NuxtLink
-              :to="{
-                name: 'category-category',
-                params: { category: archive.category },
-              }"
-              >{{ archive.category }}</NuxtLink
-            >
-          </Badge>
-        </div>
       </div>
     </div>
 
-    <!-- Created At -->
+    <!-- Added At -->
     <div
       class="flex gap-1.5 sm:grid grid-cols-6 md:grid-cols-5 2xl:grid-cols-6"
     >
-      <p class="text-md font-medium text-foreground/80">Created</p>
+      <p class="text-md font-medium text-foreground/80">Added</p>
       <div class="flex flex-wrap gap-1 col-start-2 col-span-full">
-        <Badge class="bg-primary font-normal rounded-sm hover:bg-primary/80">
+        <Badge
+          class="bg-border text-foreground font-normal rounded-sm hover:bg-border/80"
+        >
           <NuxtTime
             :datetime="archive.created_at"
             locale="en-GB"
@@ -185,7 +165,9 @@ const df = new DateFormatter("en-GB", {
     >
       <p class="text-md font-medium text-foreground/80">Updated</p>
       <div class="flex flex-wrap gap-1 col-start-2 col-span-full">
-        <Badge class="bg-primary font-normal rounded-sm hover:bg-primary/80">
+        <Badge
+          class="bg-border text-foreground font-normal rounded-sm hover:bg-border/80"
+        >
           <NuxtTime
             :datetime="archive.updated_at"
             locale="en-GB"
@@ -202,20 +184,18 @@ const df = new DateFormatter("en-GB", {
     <!-- URLs -->
     <!--TODO: URLs with icons probably-->
     <div
+      v-if="archive.url"
       class="flex gap-1.5 sm:grid grid-cols-6 md:grid-cols-5 2xl:grid-cols-6"
     >
       <p class="text-md font-medium text-foreground/80">URLs</p>
       <div class="flex flex-wrap gap-1 col-start-2 col-span-full">
-        <Badge v-if="!archive.url" class="bg-primary hover:bg-primary/80">
-          null
+        <Badge
+          v-for="url in archive.url"
+          :key="url.id"
+          class="bg-border text-foreground font-normal hover:bg-border/80"
+        >
+          <NuxtLink :to="`${url.url}`" target="_blank"> {{ url.url }}</NuxtLink>
         </Badge>
-        <div v-for="url in archive.url" v-else :key="url.id">
-          <Badge class="bg-primary font-normal hover:bg-primary/80">
-            <NuxtLink :to="`${url.url}`" target="_blank">
-              {{ url.url }}</NuxtLink
-            >
-          </Badge>
-        </div>
       </div>
     </div>
   </div>
