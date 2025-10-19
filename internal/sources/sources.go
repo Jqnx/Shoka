9e497a657749fd2e049b0b7d98d0276ef0ee6f95
Jan Van Look <jan.derpingguys@hotmail.com>
2025-10-20 00:51:06 +0200

    updated sources.go

--- internal/sources/sources.go
+++ internal/sources/sources.go
@@ -1,30 +1,41 @@
+// Package sources contains the interface for interacting with various other source packages
 package sources
 
 import (
-	"Shoka/internal/config"
-	"Shoka/internal/models"
-	"Shoka/internal/sources/nhentai"
 	"fmt"
 	"net/url"
+
+	"Shoka/internal/config"
+	"Shoka/internal/models"
+	"Shoka/internal/sources/comicinfo"
+	"Shoka/internal/sources/nhentai"
 )
 
 // TODO: custom types to use for declaring source
 
 type Sources interface {
-	Download()
-	GetMetadata(method string) ([]models.Metadata, error)
+	Download() // NOTE: Probably doable with gocron instead of asynq
+	GetMetadata() ([]models.Metadata, error)
+	SetMetadata(data any) error
 	SetURL(u *url.URL)
 	SetTitle(title string)
+	Unmarshal(data any) error
 }
 
-func NewSource(source string, cfg *config.Config) (Sources, error) {
+func NewSource(cfg *config.Config, source string, method *string) (Sources, error) {
 	switch source {
-	case config.SourceNhentai, config.SourceNhentaiSearch:
+	case config.SourceNhentai:
 		if cfg.Sources.Flaresolverr.URL == "" {
 			return nil, fmt.Errorf("this source requires flaresolverr")
 		} else {
-			return nhentai.NewNhentaiSource(cfg, source), nil
+			NHSource, err := nhentai.NewNhentaiSource(cfg, method)
+			if err != nil {
+				return nil, err
+			}
+			return NHSource, nil
 		}
+	case config.SourceComicInfo:
+		return comicinfo.NewComicInfo(), nil
 	}
 	return nil, nil
 }
