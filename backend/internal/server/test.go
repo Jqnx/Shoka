1c67deb1608b4b3a2f45bb44e7f899e1786460f1
Jan Van Look <jan.derpingguys@hotmail.com>
2025-12-28 18:11:21 +0100

    backend: cleaned test handler

--- backend/internal/server/test.go
+++ backend/internal/server/test.go
@@ -1,41 +1,8 @@
 package server
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
 
 func (s *Server) testHandler(c *gin.Context) {
-	// ctx := context.Background()
-	// id := c.Param("id")
-
-	//arch, err := s.repo.GetArchiveByID(ctx, id)
-	//if err != nil {
-	//	c.JSON(http.StatusInternalServerError, gin.H{
-	//		"error": err,
-	//	})
-	//	return
-	//}
-
-	//zip, err := fsutil.OpenArchive(arch.FilePath)
-	//if err != nil {
-	//	c.JSON(http.StatusInternalServerError, gin.H{
-	//		"error": err,
-	//	})
-	//	return
-	//}
-
-	//list, err := zip.GetFileNames(true)
-	//if err != nil {
-	//	c.JSON(http.StatusInternalServerError, gin.H{
-	//		"error": err,
-	//	})
-	//	return
-	//}
-
-	c.JSON(http.StatusOK, &gin.H{
-		"id":   c.GetString("userid"),
-		"name": c.GetString("username"),
-	})
 }
