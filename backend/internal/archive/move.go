30508d357ccb2b767304f9c7411c6484d6522633
Jan Van Look <jan.derpingguys@hotmail.com>
2026-01-28 00:27:37 +0100

    backend: fixed new directory not being created

--- backend/internal/archive/move.go
+++ backend/internal/archive/move.go
@@ -14,27 +14,24 @@ import (
 func (a *Archive) MoveOnFileUpdate() error {
 	ctx := context.Background()
 	oldHash := a.FileHash
-	// 1. Generate new hash based on updated file
+
 	a.setHash()
-	// 2. Update hash in database
-	if err := a.app.Repo.UpdateFileHash(ctx, repository.UpdateFileHashParams{
-		FileHash:  a.FileHash,
-		ID:        a.ID,
-		UpdatedAt: time.Now(),
-	}); err != nil {
-		return err
-	}
-	// 3. Concatenate new ThumbPath based on newly generated hash
+
 	d := fsutil.NewArchiveDir(a.app.Cfg.ThumbDir, a.FileHash)
 	newPath, err := d.GenThumbDir()
 	if err != nil {
 		return err
 	}
-	// 4. Rename old ThumbPath to new ThumbPath
+
+	newPathParent := filepath.Dir(newPath)
+	if err := os.MkdirAll(newPathParent, 0o755); err != nil {
+		return err
+	}
+
 	if err := os.Rename(*a.ThumbPath, newPath); err != nil {
 		return err
 	}
-	// 5. Update ThumbPath in database
+
 	if err := a.app.Repo.UpdateThumbPath(ctx, repository.UpdateThumbPathParams{
 		ThumbPath: &newPath,
 		ID:        a.ID,
@@ -42,12 +39,20 @@ func (a *Archive) MoveOnFileUpdate() error {
 	}); err != nil {
 		return err
 	}
-	// 6. Update cover name to newly generated hash
+
 	oldCover := fmt.Sprintf("%s.webp", oldHash)
 	newCover := fmt.Sprintf("%s.webp", a.FileHash)
 	oldCoverPath := filepath.Join(newPath, "cover", oldCover)
 	newCoverPath := filepath.Join(newPath, "cover", newCover)
 	if err := os.Rename(oldCoverPath, newCoverPath); err != nil {
+		return err
+	}
+
+	if err := a.app.Repo.UpdateFileHash(ctx, repository.UpdateFileHashParams{
+		FileHash:  a.FileHash,
+		ID:        a.ID,
+		UpdatedAt: time.Now(),
+	}); err != nil {
 		return err
 	}
 
