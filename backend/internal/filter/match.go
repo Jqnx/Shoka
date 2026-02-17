fe21aa5e4eadc6cc147bc2843910de747644ad85
Jan Van Look <jan.derpingguys@hotmail.com>
2026-02-17 17:18:24 +0100

    backend: moved matching and fetching of filters into 1 function

--- backend/internal/filter/match.go
+++ backend/internal/filter/match.go
@@ -10,116 +10,106 @@ import (
 	"github.com/google/uuid"
 )
 
-type ArchiveList struct {
-	Archives []repository.GetArchiveFilterSortListRow `json:"archives"`
-	Count    int                                      `json:"total"`
-}
-
-type FavoriteArchiveList struct {
-	Archives []repository.GetFavoriteArchiveFilterSortListRow `json:"archives"`
-	Count    int64                                            `json:"total"`
-}
-
-func matchArtistsTags(in models.ArchiveFilters, qtx *repository.Queries) ([]string, error) {
-	tags, err := fetchTags(in.Tags, qtx)
-	if err != nil {
-		return nil, err
-	}
-	artists, err := fetchArtists(in.Artists, qtx)
-	if err != nil {
-		return nil, err
-	}
-
-	matched := util.MatchStringsInSlices(tags, artists)
-	return matched, nil
-}
-
-func matchCharactersParodies(in models.ArchiveFilters, qtx *repository.Queries) ([]string, error) {
-	characters, err := fetchCharacters(in.Characters, qtx)
-	if err != nil {
-		return nil, err
-	}
-	parodies, err := fetchParodies(in.Parodies, qtx)
-	if err != nil {
-		return nil, err
-	}
-
-	matched := util.MatchStringsInSlices(characters, parodies)
-	return matched, nil
-}
-
-func matchLanguagesCategories(in models.ArchiveFilters, qtx *repository.Queries) ([]string, error) {
-	languages, err := fetchLanguages(in.Languages, qtx)
-	if err != nil {
-		return nil, err
-	}
-	categories, err := fetchCategories(in.Categories, qtx)
-	if err != nil {
-		return nil, err
-	}
-
-	matched := util.MatchStringsInSlices(languages, categories)
-	return matched, nil
+type MatchAndGetParams struct {
+	Filters  models.ArchiveFilters
+	Page     int
+	PageSize int
+	Order    string
+	UserID   uuid.UUID
 }
 
 func Match(in models.ArchiveFilters, qtx *repository.Queries) ([]string, error) {
-	artistsTags, err := matchArtistsTags(in, qtx)
-	if err != nil {
-		return nil, err
-	}
-	charactersParodies, err := matchCharactersParodies(in, qtx)
-	if err != nil {
-		return nil, err
-	}
-	languagesCategories, err := matchLanguagesCategories(in, qtx)
-	if err != nil {
-		return nil, err
-	}
-
-	aTcP := util.MatchStringsInSlices(artistsTags, charactersParodies)
-
-	match := util.MatchStringsInSlices(aTcP, languagesCategories)
-	return match, nil
-}
-
-func MatchAndGet(in models.ArchiveFilters, qtx *repository.Queries, page, pageSize int, order string, uid uuid.UUID) (*ArchiveList, error) {
 	ctx := context.Background()
-	list, err := Match(in, qtx)
-	if err != nil {
-		return nil, err
-	}
 
-	archives, err := qtx.GetArchiveFilterSortList(ctx, repository.GetArchiveFilterSortListParams{
-		Uid:     uid,
-		Ids:     list,
-		OrderBy: order,
-		Limit:   int32(pageSize),
-		Offset:  (int32(page) - 1) * int32(pageSize),
+	idsByArtists, err := qtx.GetArchiveIDsByArtists(ctx, repository.GetArchiveIDsByArtistsParams{
+		Artists: in.Artists,
+		Amount:  int32(len(in.Artists)),
 	})
 	if err != nil {
 		return nil, err
 	}
 
-	results := &ArchiveList{
-		Archives: archives,
-		Count:    len(list),
+	idsByCategory, err := qtx.GetArchiveIDsByCategories(ctx, repository.GetArchiveIDsByCategoriesParams{
+		Categories: in.Categories,
+		Amount:     int32(len(in.Categories)),
+	})
+	if err != nil {
+		return nil, err
 	}
-	return results, nil
+
+	idsByCharacters, err := qtx.GetArchiveIDsByCharacters(ctx, repository.GetArchiveIDsByCharactersParams{
+		Characters: in.Characters,
+		Amount:     int32(len(in.Characters)),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	idsByLanguages, err := qtx.GetArchiveIDsByLanguages(ctx, repository.GetArchiveIDsByLanguagesParams{
+		Languages: in.Languages,
+		Amount:    int32(len(in.Languages)),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	idsByParodies, err := qtx.GetArchiveIDsByParodies(ctx, repository.GetArchiveIDsByParodiesParams{
+		Parodies: in.Parodies,
+		Amount:   int32(len(in.Parodies)),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	idsByTags, err := qtx.GetArchiveIDsByTags(ctx, repository.GetArchiveIDsByTagsParams{
+		Tags:   in.Tags,
+		Amount: int32(len(in.Tags)),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	match := util.Intersect(idsByArtists, idsByCategory, idsByCharacters, idsByLanguages, idsByParodies, idsByTags)
+	return match, nil
 }
 
-func MatchAndGetFavorites(in models.ArchiveFilters, qtx *repository.Queries, page, pageSize int, userid uuid.UUID, order string) (*FavoriteArchiveList, error) {
+func MatchAndGet(qtx *repository.Queries, arg MatchAndGetParams) (*models.ArchiveListResponse[repository.GetArchiveFilterSortListRow], error) {
 	ctx := context.Background()
-	list, err := Match(in, qtx)
+	list, err := Match(arg.Filters, qtx)
+	if err != nil {
+		return nil, err
+	}
+
+	archives, err := qtx.GetArchiveFilterSortList(ctx, repository.GetArchiveFilterSortListParams{
+		Uid:     arg.UserID,
+		Ids:     list,
+		OrderBy: arg.Order,
+		Limit:   int32(arg.PageSize),
+		Offset:  (int32(arg.Page) - 1) * int32(arg.PageSize),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &models.ArchiveListResponse[repository.GetArchiveFilterSortListRow]{
+		Archives: archives,
+		Count:    len(list),
+	}, nil
+}
+
+func MatchAndGetFavorites(qtx *repository.Queries, arg MatchAndGetParams) (*models.ArchiveListResponse[repository.GetFavoriteArchiveFilterSortListRow], error) {
+	ctx := context.Background()
+	list, err := Match(arg.Filters, qtx)
 	if err != nil {
 		return nil, err
 	}
 
 	archives, err := qtx.GetFavoriteArchiveFilterSortList(ctx, repository.GetFavoriteArchiveFilterSortListParams{
-		UserID:  userid,
+		UserID:  arg.UserID,
 		Ids:     list,
-		OrderBy: order,
-		Limit:   int32(pageSize),
-		Offset:  (int32(page) - 1) * int32(pageSize),
+		OrderBy: arg.Order,
+		Limit:   int32(arg.PageSize),
+		Offset:  (int32(arg.Page) - 1) * int32(arg.PageSize),
 	})
 	if err != nil {
 		return nil, err
@@ -127,15 +117,15 @@ func MatchAndGetFavorites(in models.ArchiveFilters, qtx *repository.Queries, pag
 
 	count, err := qtx.CountFavoriteFilteredArchive(ctx, repository.CountFavoriteFilteredArchiveParams{
 		Ids:    list,
-		UserID: userid,
+		UserID: arg.UserID,
 	})
 	if err != nil {
 		return nil, err
 	}
 
-	results := &FavoriteArchiveList{
+	results := &models.ArchiveListResponse[repository.GetFavoriteArchiveFilterSortListRow]{
 		Archives: archives,
-		Count:    count,
+		Count:    int(count),
 	}
 
 	return results, nil
--- backend/internal/server/archive_filter.go
+++ backend/internal/server/archive_filter.go
@@ -2,43 +2,23 @@ package server
 
 import (
 	"context"
-	"errors"
+	"fmt"
 	"net/http"
-	"reflect"
 	"strconv"
 
-	"Shoka/internal/config"
 	"Shoka/internal/filter"
 	"Shoka/internal/models"
 	"Shoka/internal/repository"
 	"Shoka/internal/util"
 
 	"github.com/gin-gonic/gin"
-	"github.com/go-playground/validator/v10"
 	"github.com/google/uuid"
 )
 
-// TODO: Maybe filter/sort all function for when there is no page or pagesize given.
-
+// TODO: Add metadata to individual archives, so basically the normal get for 1 archive but for a list
 func (s *Server) getArchiveFilterHandler(c *gin.Context) {
-	var payload models.ArchiveFilters
-	if errPost := c.ShouldBind(&payload); errPost != nil {
-		var verr validator.ValidationErrors
-		if errors.As(errPost, &verr) {
-			c.JSON(http.StatusBadRequest, &models.ResponseFail{
-				Status: "fail",
-				Data:   config.Validate(verr),
-			})
-			return
-		}
-		c.JSON(http.StatusBadRequest, &models.ResponseFail{
-			Status: "fail",
-			Data:   errPost.Error(),
-		})
-		return
-	}
-
 	ctx := context.Background()
+	filters, hasFilter := filter.GetFromQuery(c)
 
 	var uid uuid.UUID
 	header := c.Request.Header.Get("Authorization")
@@ -46,166 +26,35 @@ func (s *Server) getArchiveFilterHandler(c *gin.Context) {
 		uid = util.GetUserFromRequest(c)
 	}
 
-	// URL Queries
-	p := c.Query("page")
-	ps := c.Query("size")
-	sortby := c.Query("sortby")
-	sortdir := c.Query("sortdir")
-
-	// Set Page & Page Size
-	page, _ := strconv.Atoi(p)
+	order := fmt.Sprintf("%s_%s", c.Query("sortby"), c.Query("sortdir"))
+	page, _ := strconv.Atoi(c.Query("page"))
 	if page == 0 {
 		page = 1
 	}
-	pageSize, _ := strconv.Atoi(ps)
+	pageSize, _ := strconv.Atoi(c.Query("size"))
 	if pageSize == 0 {
 		pageSize = 10
 	}
 
 	// Query with filters if query is not empty
-	if !reflect.DeepEqual(models.ArchiveFilters{
-		Tags:       []string{},
-		Artists:    []string{},
-		Characters: []string{},
-		Parodies:   []string{},
-		Languages:  []string{},
-		Categories: []string{},
-	}, payload) {
-		switch sortby {
-		case "title":
-			if sortdir == "desc" {
-				archives, err := filter.MatchAndGet(payload, s.repo, page, pageSize, "title_desc", uid)
-				if err != nil {
-					c.JSON(http.StatusInternalServerError, &models.Response{
-						Status:  "error",
-						Message: err.Error(),
-					})
-					return
-				}
-				c.JSON(http.StatusOK, archives)
-				return
-			} else {
-				archives, err := filter.MatchAndGet(payload, s.repo, page, pageSize, "title_asc", uid)
-				if err != nil {
-					c.JSON(http.StatusInternalServerError, &models.Response{
-						Status:  "error",
-						Message: err.Error(),
-					})
-					return
-				}
-				c.JSON(http.StatusOK, archives)
-				return
-			}
-		case "page_count":
-			if sortdir == "desc" {
-				archives, err := filter.MatchAndGet(payload, s.repo, page, pageSize, "page_count_desc", uid)
-				if err != nil {
-					c.JSON(http.StatusInternalServerError, &models.Response{
-						Status:  "error",
-						Message: err.Error(),
-					})
-					return
-				}
-				c.JSON(http.StatusOK, archives)
-				return
-			} else {
-				archives, err := filter.MatchAndGet(payload, s.repo, page, pageSize, "page_count_asc", uid)
-				if err != nil {
-					c.JSON(http.StatusInternalServerError, &models.Response{
-						Status:  "error",
-						Message: err.Error(),
-					})
-					return
-				}
-				c.JSON(http.StatusOK, archives)
-				return
-			}
-		case "created_at":
-			if sortdir == "desc" {
-				archives, err := filter.MatchAndGet(payload, s.repo, page, pageSize, "created_at_desc", uid)
-				if err != nil {
-					c.JSON(http.StatusInternalServerError, &models.Response{
-						Status:  "error",
-						Message: err.Error(),
-					})
-					return
-				}
-				c.JSON(http.StatusOK, archives)
-				return
-			} else {
-				archives, err := filter.MatchAndGet(payload, s.repo, page, pageSize, "created_at_asc", uid)
-				if err != nil {
-					c.JSON(http.StatusInternalServerError, &models.Response{
-						Status:  "error",
-						Message: err.Error(),
-					})
-					return
-				}
-				c.JSON(http.StatusOK, archives)
-				return
-			}
-		case "updated_at":
-			if sortdir == "desc" {
-				archives, err := filter.MatchAndGet(payload, s.repo, page, pageSize, "updated_at_desc", uid)
-				if err != nil {
-					c.JSON(http.StatusInternalServerError, &models.Response{
-						Status:  "error",
-						Message: err.Error(),
-					})
-					return
-				}
-				c.JSON(http.StatusOK, archives)
-				return
-			} else {
-				archives, err := filter.MatchAndGet(payload, s.repo, page, pageSize, "updated_at_asc", uid)
-				if err != nil {
-					c.JSON(http.StatusInternalServerError, &models.Response{
-						Status:  "error",
-						Message: err.Error(),
-					})
-					return
-				}
-				c.JSON(http.StatusOK, archives)
-				return
-			}
-		case "release_date":
-			if sortdir == "asc" {
-				archives, err := filter.MatchAndGet(payload, s.repo, page, pageSize, "release_date_asc", uid)
-				if err != nil {
-					c.JSON(http.StatusInternalServerError, &models.Response{
-						Status:  "error",
-						Message: err.Error(),
-					})
-					return
-				}
-				c.JSON(http.StatusOK, archives)
-				return
-			} else {
-				archives, err := filter.MatchAndGet(payload, s.repo, page, pageSize, "release_date_desc", uid)
-				if err != nil {
-					c.JSON(http.StatusInternalServerError, &models.Response{
-						Status:  "error",
-						Message: err.Error(),
-					})
-					return
-				}
-				c.JSON(http.StatusOK, archives)
-				return
-			}
-		default:
-			archives, err := filter.MatchAndGet(payload, s.repo, page, pageSize, "title_asc", uid)
-			if err != nil {
-				c.JSON(http.StatusInternalServerError, &models.Response{
-					Status:  "error",
-					Message: err.Error(),
-				})
-				return
-			}
-			c.JSON(http.StatusOK, archives)
+	if hasFilter {
+		archives, err := filter.MatchAndGet(s.repo, filter.MatchAndGetParams{
+			Filters:  *filters,
+			Page:     page,
+			PageSize: pageSize,
+			Order:    order,
+			UserID:   uid,
+		})
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, &models.Response{
+				Status:  "error",
+				Message: err.Error(),
+			})
 			return
 		}
+		c.JSON(http.StatusOK, archives)
+		return
 	} else {
-		// Otherwise only sort
 		count, err := s.repo.CountArchives(ctx)
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, &models.Response{
@@ -214,231 +63,27 @@ func (s *Server) getArchiveFilterHandler(c *gin.Context) {
 			})
 			return
 		}
-		switch sortby {
-		case "title":
-			if sortdir == "desc" {
-				archives, err := s.repo.GetArchiveSortList(ctx, repository.GetArchiveSortListParams{
-					Uid:     uid,
-					OrderBy: "title_desc",
-					Limit:   int32(pageSize),
-					Offset:  (int32(page) - 1) * int32(pageSize),
-				})
-				if err != nil {
-					c.JSON(http.StatusInternalServerError, &models.Response{
-						Status:  "error",
-						Message: err.Error(),
-					})
-					return
-				}
-				c.JSON(http.StatusOK, gin.H{
-					"archives": archives,
-					"total":    count,
-				})
-				return
-			} else {
-				archives, err := s.repo.GetArchiveSortList(ctx, repository.GetArchiveSortListParams{
-					Uid:     uid,
-					OrderBy: "title_asc",
-					Limit:   int32(pageSize),
-					Offset:  (int32(page) - 1) * int32(pageSize),
-				})
-				if err != nil {
-					c.JSON(http.StatusInternalServerError, &models.Response{
-						Status:  "error",
-						Message: err.Error(),
-					})
-					return
-				}
-				c.JSON(http.StatusOK, gin.H{
-					"archives": archives,
-					"total":    count,
-				})
-				return
-			}
-		case "page_count":
-			if sortdir == "desc" {
-				archives, err := s.repo.GetArchiveSortList(ctx, repository.GetArchiveSortListParams{
-					Uid:     uid,
-					OrderBy: "page_count_desc",
-					Limit:   int32(pageSize),
-					Offset:  (int32(page) - 1) * int32(pageSize),
-				})
-				if err != nil {
-					c.JSON(http.StatusInternalServerError, &models.Response{
-						Status:  "error",
-						Message: err.Error(),
-					})
-					return
-				}
-				c.JSON(http.StatusOK, gin.H{
-					"archives": archives,
-					"total":    count,
-				})
-				return
-			} else {
-				archives, err := s.repo.GetArchiveSortList(ctx, repository.GetArchiveSortListParams{
-					Uid:     uid,
-					OrderBy: "page_count_asc",
-					Limit:   int32(pageSize),
-					Offset:  (int32(page) - 1) * int32(pageSize),
-				})
-				if err != nil {
-					c.JSON(http.StatusInternalServerError, &models.Response{
-						Status:  "error",
-						Message: err.Error(),
-					})
-					return
-				}
-				c.JSON(http.StatusOK, gin.H{
-					"archives": archives,
-					"total":    count,
-				})
-				return
-			}
-		case "created_at":
-			if sortdir == "desc" {
-				archives, err := s.repo.GetArchiveSortList(ctx, repository.GetArchiveSortListParams{
-					Uid:     uid,
-					OrderBy: "created_at_desc",
-					Limit:   int32(pageSize),
-					Offset:  (int32(page) - 1) * int32(pageSize),
-				})
-				if err != nil {
-					c.JSON(http.StatusInternalServerError, &models.Response{
-						Status:  "error",
-						Message: err.Error(),
-					})
-					return
-				}
-				c.JSON(http.StatusOK, gin.H{
-					"archives": archives,
-					"total":    count,
-				})
-				return
-			} else {
-				archives, err := s.repo.GetArchiveSortList(ctx, repository.GetArchiveSortListParams{
-					Uid:     uid,
-					OrderBy: "created_at_asc",
-					Limit:   int32(pageSize),
-					Offset:  (int32(page) - 1) * int32(pageSize),
-				})
-				if err != nil {
-					c.JSON(http.StatusInternalServerError, &models.Response{
-						Status:  "error",
-						Message: err.Error(),
-					})
-					return
-				}
-				c.JSON(http.StatusOK, gin.H{
-					"archives": archives,
-					"total":    count,
-				})
-				return
-			}
-		case "updated_at":
-			if sortdir == "desc" {
-				archives, err := s.repo.GetArchiveSortList(ctx, repository.GetArchiveSortListParams{
-					Uid:     uid,
-					OrderBy: "updated_at_desc",
-					Limit:   int32(pageSize),
-					Offset:  (int32(page) - 1) * int32(pageSize),
-				})
-				if err != nil {
-					c.JSON(http.StatusInternalServerError, &models.Response{
-						Status:  "error",
-						Message: err.Error(),
-					})
-					return
-				}
-				c.JSON(http.StatusOK, gin.H{
-					"archives": archives,
-					"total":    count,
-				})
-				return
-			} else {
-				archives, err := s.repo.GetArchiveSortList(ctx, repository.GetArchiveSortListParams{
-					Uid:     uid,
-					OrderBy: "updated_at_asc",
-					Limit:   int32(pageSize),
-					Offset:  (int32(page) - 1) * int32(pageSize),
-				})
-				if err != nil {
-					c.JSON(http.StatusInternalServerError, &models.Response{
-						Status:  "error",
-						Message: err.Error(),
-					})
-					return
-				}
-				c.JSON(http.StatusOK, gin.H{
-					"archives": archives,
-					"total":    count,
-				})
-				return
-			}
-		case "release_date":
-			if sortdir == "asc" {
-				archives, err := s.repo.GetArchiveSortList(ctx, repository.GetArchiveSortListParams{
-					Uid:     uid,
-					OrderBy: "release_date_asc",
-					Limit:   int32(pageSize),
-					Offset:  (int32(page) - 1) * int32(pageSize),
-				})
-				if err != nil {
-					c.JSON(http.StatusInternalServerError, &models.Response{
-						Status:  "error",
-						Message: err.Error(),
-					})
-					return
-				}
-				c.JSON(http.StatusOK, gin.H{
-					"archives": archives,
-					"total":    count,
-				})
-				return
-			} else {
-				archives, err := s.repo.GetArchiveSortList(ctx, repository.GetArchiveSortListParams{
-					Uid:     uid,
-					OrderBy: "release_date_desc",
-					Limit:   int32(pageSize),
-					Offset:  (int32(page) - 1) * int32(pageSize),
-				})
-				if err != nil {
-					c.JSON(http.StatusInternalServerError, &models.Response{
-						Status:  "error",
-						Message: err.Error(),
-					})
-					return
-				}
-				c.JSON(http.StatusOK, gin.H{
-					"archives": archives,
-					"total":    count,
-				})
-				return
-			}
-		default:
-			archives, err := s.repo.GetArchiveSortList(ctx, repository.GetArchiveSortListParams{
-				Uid:     uid,
-				OrderBy: "title_asc",
-				Limit:   int32(pageSize),
-				Offset:  (int32(page) - 1) * int32(pageSize),
-			})
-			if err != nil {
-				c.JSON(http.StatusInternalServerError, &models.Response{
-					Status:  "error",
-					Message: err.Error(),
-				})
-				return
-			}
-			c.JSON(http.StatusOK, gin.H{
-				"archives": archives,
-				"total":    count,
+		archives, err := s.repo.GetArchiveSortList(ctx, repository.GetArchiveSortListParams{
+			Uid:     uid,
+			OrderBy: order,
+			Limit:   int32(pageSize),
+			Offset:  (int32(page) - 1) * int32(pageSize),
+		})
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, &models.Response{
+				Status:  "error",
+				Message: err.Error(),
 			})
 			return
 		}
+		c.JSON(http.StatusOK, &models.ArchiveListResponse[repository.GetArchiveSortListRow]{
+			Archives: archives,
+			Count:    int(count),
+		})
+		return
 	}
 }
 
-// FIX:  duplicate (category, language) entries in response
 func (s *Server) getAllFiltersHandler(c *gin.Context) {
 	ctx := context.Background()
 
