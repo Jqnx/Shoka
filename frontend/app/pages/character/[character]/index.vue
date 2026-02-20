f20614a53b1cca2a2d9acb2973994377ad06b421
Jan Van Look <jan.derpingguys@hotmail.com>
2026-02-20 15:53:05 +0100

    frontend: updated pages to support sorting

--- frontend/app/pages/character/[character]/index.vue
+++ frontend/app/pages/character/[character]/index.vue
@@ -11,29 +11,53 @@ import {
 } from "@/components/ui/pagination";
 
 const { character } = useRoute().params;
-const token = await useAuth().getToken();
-
-const currentPage = ref(0);
-const query = useRoute().query;
-const page = parseInt(query.page as string, 10);
-if (isNaN(page)) {
-  currentPage.value = 1;
-} else {
-  currentPage.value = page;
-}
-
-const pageSize = useRuntimeConfig().public.pageSize;
-
 useHead({
   title: `Character: ${character}`,
+});
+
+const token = await useAuth().getToken();
+const router = useRouter();
+const route = useRoute();
+const { SortOptions: sortList, SortOptionsStore } = useSortOptions();
+
+const sortBy =
+  route.query.sortby != undefined ? ref(route.query.sortby) : ref("title");
+const sortDir =
+  route.query.sortdir != undefined ? ref(route.query.sortdir) : ref("asc");
+const sortLabel =
+  route.query.sortby != undefined
+    ? ref(SortOptionsStore[sortBy.value as string])
+    : ref("Title");
+const page = parseInt(route.query.page as string, 10);
+const currentPage = ref(isNaN(page) ? 1 : page);
+const pageSize = ref(useRuntimeConfig().public.pageSize);
+
+const siblingCount = computed(() => {
+  return useDevice().isMobile ? 0 : 1;
 });
 
 const { data: archives } = await useFetch(`/api/character/${character}`, {
   onRequest({ options }) {
     options.headers.set("Authorization", `Bearer ${token}`);
   },
-  query: { page: currentPage, size: pageSize },
+  query: {
+    page: currentPage,
+    size: pageSize,
+    sortby: sortBy,
+    sortdir: sortDir,
+  },
   key: `archives-${character}`,
+});
+
+watch([currentPage, sortBy, sortDir], () => {
+  router.push({
+    query: {
+      page: currentPage.value,
+      size: pageSize.value,
+      sortby: sortBy.value,
+      sortdir: sortDir.value,
+    },
+  });
 });
 
 const scrollToTop = () => {
@@ -42,10 +66,15 @@ const scrollToTop = () => {
 </script>
 
 <template>
-  <div class="flex flex-1 flex-col gap-4 p-4 pt-0">
-    <!--TODO: Filter options here -->
+  <div class="flex flex-1 flex-col gap-2">
+    <ListOptions
+      v-model:sort-by="sortBy"
+      v-model:sort-dir="sortDir"
+      v-model:sort-label="sortLabel"
+      :sort-list="sortList"
+    />
     <div
-      class="py-4 grid gap-4 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6"
+      class="px-2 py-2 grid gap-2 xl:gap-4 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6"
     >
       <div v-for="archive in archives.archives" :key="archive.id">
         <GalleryItem
@@ -59,11 +88,10 @@ const scrollToTop = () => {
     <Pagination
       v-model:page="currentPage"
       :show-edges="true"
-      :sibling-count="2"
+      :sibling-count="siblingCount"
       :items-per-page="pageSize"
       :total="archives.total"
       :default-page="1"
-      @update:page="navigateTo(`/a?page=${currentPage}`)"
     >
       <PaginationContent v-slot="{ items }">
         <PaginationFirst @click="scrollToTop" />
